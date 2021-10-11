8c94038935bc63269395d0bea7707510e712f4cc
Sam Melanson <samuelmelanson@live.com>
2021-10-10 22:48:24 -0400

    courseinfo 1.5 complete

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Content from './Content'
 
-function App() {
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts : [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header title={course.name}/>
+      <Content parts={course.parts} />
+      <Footer text={`Number of exercises ${course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}`}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

0d4c6f62561250503c01619167d23ea40ab8a401
Sam Melanson <samuelmelanson@live.com>
2021-10-18 11:50:15 -0400

    part4.14 bloglistexpansion step2

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -16,8 +16,30 @@ blogRouter.post('/', async (req, res) => {
   res.status(201).json(result)
 })
 
+blogRouter.put('/:id', async (req, res) => {
+  const { author, title, likes } = req.body
+  if(!author || !title || !likes) {
+    return res.status(400).json({ error: 'missing blog data {[author], [title], [likes]} required' })
+  }
+  const blog = { author, title, likes }
+
+  const response = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+  if(response === null) {
+    return res.status(404).send({ error:'not found' })
+  }
+  res.send(response)
+})
+
 blogRouter.get('/:id', async (req, res) => {
   const response = await Blog.findById(req.params.id)
+  res.json(response)
+})
+
+blogRouter.delete('/:id', async (req, res) => {
+  const response = await Blog.findByIdAndRemove(req.params.id)
+  if(response === null) {
+    return res.status(404).send({ error:'not found' })
+  }
   res.json(response)
 })
 
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -20,6 +20,14 @@ const initialBlogs = [
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
     __v: 0
+  },
+  {
+    _id: '6169adae4306900da455af35',
+    title: 'Adding an additional test blog',
+    author: 'Me',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 25,
+    __v: 0
   }
 ]
 
@@ -39,7 +47,7 @@ describe('when blogs are initally loaded', () => {
   test('that there are the correct number of initial blogs', async () => {
     const response = await api.get('/api/blogs')
     const blogs = response.body
-    expect(blogs.length).toBe(initialBlogs.length)
+    expect(blogs).toHaveLength(initialBlogs.length)
   })
 
   test('that the _id field is properly set to id when parsing the mongo data', async () => {
@@ -55,7 +63,7 @@ describe('addition of a new note', () => {
     await api.post('/api/blogs').send(newBlog).expect(201)
 
     const blogsAfterUpdate = await api.get('/api/blogs')
-    expect(blogsAfterUpdate.body.length).toBe(initialBlogs.length + 1)
+    expect(blogsAfterUpdate.body).toHaveLength(initialBlogs.length + 1)
   })
 
   test('that the new blog post has a default value of 0 likes', async () => {
@@ -67,6 +75,44 @@ describe('addition of a new note', () => {
 
   test('that requests without an author and title result in a 400', async () => {
     await api.post('/api/blogs').send({}).expect(400)
+  })
+})
+
+describe('deleting a blog post', () => {
+  test('that the server responds with a 404 when given an unknow id', async () => {
+    await api.delete('/api/blogs/6169adae4306900da455af33').expect(404)
+  })
+
+  test('that the server responds with a 400 on invalid/malformatted ids', async () => {
+    await api.delete('/api/blogs/NotARealId').expect(400)
+  })
+
+  test('that a blog post is indeed being removed', async () => {
+    await api.delete(`/api/blogs/${initialBlogs[1]._id}`).expect(200)
+
+    const response = await api.get('/api/blogs').expect(200)
+    const blogsAfterDelete = response.body
+    expect(blogsAfterDelete).toHaveLength(initialBlogs.length - 1)
+  })
+})
+
+describe('updating a blog post', () => {
+  test('that the server responds with a 404 when given an unknow id', async () => {
+    await api.put('/api/blogs/6169adae4306900da455af33').send({ author:'updator', title:'updated title', likes:1 }).expect(404)
+  })
+
+  test('that the server responds with a 400 on invalid/malformatted ids', async () => {
+    await api.put('/api/blogs/notARealId').send({ author:'updator', title:'updated title', likes:1 }).expect(400)
+  })
+  test('that if there is missing info for the blog, respond with 400', async () => {
+    await api.put(`/api/blogs/${initialBlogs[0]._id}`, {}, { new: true }).expect(400)
+  })
+  test('that the blog post is being correctly updated', async () => {
+    const { author, title, likes } = initialBlogs[0]
+    const newBlog = { author, title, likes: likes + 1 }
+    console.log({ newBlog, id: initialBlogs[0]._id })
+    const response = await api.put(`/api/blogs/${initialBlogs[0]._id}`).send(newBlog).expect(200)
+    expect(response.body.likes).toBe(likes + 1)
   })
 })
 

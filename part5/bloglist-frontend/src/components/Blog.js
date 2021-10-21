92b734a6b641052afd7aef38afb6214fd07a5a47
Sam Melanson <samuelmelanson@live.com>
2021-10-21 16:51:16 -0400

    part5.7 bloglistfrontend step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) =>   {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div className="blog-container">
+      <div>
+        {blog.title}
+        <button onClick={() => setVisible(!visible)}>{visible ? 'hide':'show'}</button>
+      </div>
+      {visible && <div>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+      </div>}
+    </div>)
+}
 
 export default Blog

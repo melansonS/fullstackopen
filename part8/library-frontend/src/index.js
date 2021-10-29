f3776bb7674e92f96c8304a1f5df242002b64d2f
Sam Melanson <samuelmelanson@live.com>
2021-10-29 12:46:36 -0400

    part8.8 authorsview

---
+++ part8/library-frontend/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+import { ApolloClient, HttpLink, InMemoryCache, gql, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:4000"
+  })
+})
+
+const query = gql`
+query {
+  allAuthors {
+    name
+    bookCount
+    born
+  }
+}
+`
+
+client.query({query})
+  .then(response => console.log(response))
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+), document.getElementById('root'))
